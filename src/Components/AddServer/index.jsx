5e848d0714caadb7e3d601f7dfde5bc6ad9a0d65
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-21 18:30:55 +0530

    add servers

--- src/Components/AddServer/index.jsx
+++ src/Components/AddServer/index.jsx
@@ -1,14 +1,40 @@
-import { Button, Divider, Form, Input, Modal } from "antd";
-import { useState } from "react";
+import { Button, Form, Input, Modal } from "antd";
+import { useEffect, useState } from "react";
+
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import ServerPfpUpload from "../ServerPfpUpload";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+
+const db = getFirestore();
+const auth = getAuth()
 
 function AddServer(props) {
   const { visible, setMenuVisible } = props;
   const [name, setName] = useState("");
   const [pfp, setPfp] = useState("");
-
   const [invite, setInvite] = useState("");
 
-  const [current, setCurrent] = useState(null);
+  const [docId, setDocId] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (visible) {
+      const docRef = doc(collection(db, "chat"));
+      const docId = docRef.id;
+      setDocId(docId);
+      console.log(docId);
+    }
+  }, [visible]);
+
+  useEffect(() => {
+    if (!visible) {
+      setName("");
+      setPfp("");
+      setInvite("");
+    }
+  }, [visible]);
 
   return (
     <>
@@ -16,6 +42,7 @@ function AddServer(props) {
         centered
         title="Add Server"
         visible={visible}
+        onCancel={() => setMenuVisible(false)}
         footer={
           <>
             <p
@@ -29,27 +56,50 @@ function AddServer(props) {
           </>
         }
       >
-        <Form>
+        <Form layout="vertical">
+          <Form.Item label="Avatar">
+            {docId && <ServerPfpUpload serverId={docId} />}
+          </Form.Item>
           <Form.Item label="Server Name">
             <Input
               value={name}
+              required
               onChange={(e) => setName(e.target.value)}
               placeholder="Server Name"
             />
           </Form.Item>
-          <Form.Item label="Server Invite">
-            <Input
-              value={invite}
-              onChange={(e) => setInvite(e.target.value)}
-              placeholder="Server Invite"
-            />
-          </Form.Item>
-          <Form.Item label="Server Icon">
-            <Input
-              value={pfp}
-              onChange={(e) => setPfp(e.target.value)}
-              placeholder="Server Icon"
-            />
+          {
+            //submit form
+          }
+          <Form.Item>
+            <Button
+              type="primary"
+              onClick={async () => {
+                const docRef = doc(collection(db, "chat"), docId);
+                const data = {
+                  name: name,
+                  pfp: pfp,
+                  invite: docId,
+                  id: docId,
+                };
+                await setDoc(docRef, data);
+                await setDoc(doc(db, "u", auth.currentUser.uid, "chats", docId), { id: docId, name: name, pfp: pfp});
+                await setDoc(
+                  doc(
+                    db,
+                    "chat",
+                    docId,
+                    "members",
+                    auth.currentUser.uid
+                  ), {id: auth.currentUser.uid }
+                )
+                navigate(`/${docId}`);
+                setMenuVisible(false);
+              }
+              }
+            >
+              Create Server
+            </Button>
           </Form.Item>
         </Form>
       </Modal>
---
+++ src/Components/ServerPfpUpload/index.jsx
@@ -0,0 +1,106 @@
+import "./index.css";
+
+import { Avatar, Button, notification } from "antd";
+import { useState, useEffect } from "react";
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
+import { doc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
+import { EditOutlined } from "@ant-design/icons";
+
+const db = getFirestore();
+const storage = getStorage();
+
+function AvatarUpload(props) {
+  const { serverId } = props;
+  console.log(serverId)
+  const [avatar, setAvatar] = useState("");
+  const [srvDoc] = useDocumentOnce(doc(db, "chat", serverId));
+
+  useEffect(() => {
+    if (srvDoc && srvDoc.data) {
+      console.log(srvDoc)
+      if (srvDoc.data().pfp) {
+        setAvatar(srvDoc.data().pfp);
+      } else {
+        setAvatar(
+          `https://avatars.dicebear.com/api/bottts/${serverId}.svg`
+        );
+      }
+    }
+    else {
+      console.log("no doc")
+      setDoc(doc(db, "chat", serverId), {
+        pfp: avatar,
+      });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [srvDoc]);
+
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    if (file) {
+      const profileRef = ref(
+        storage,
+        `c/${serverId}/avatar.${file.name.split(".").pop()}`
+      );
+      notification["info"]({
+        message: "Uploading",
+        description: "Uploading avatar...",
+      });
+      uploadBytes(profileRef, file).then(() => {
+        getDownloadURL(profileRef).then((url) => {
+          let data = {
+              pfp: url,
+            };
+          const docRef = doc(db, "chat", serverId);
+          updateDoc(docRef, data).then(() => {
+            setAvatar(url);
+            notification["success"]({
+              message: "Uploaded",
+              description: "Avatar uploaded successfully!",
+            });
+          })
+        });
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [file]);
+
+  const onClick = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
+  };
+
+  return (
+    <>
+      <div className="AvatarContainer">
+        <div className="AvatarOverlay">
+          <input
+            type="file"
+            onChange={onClick}
+            onClick={onClick}
+            className="infile"
+            id="infile"
+            style={{
+              display: "none",
+            }}
+          />
+          <Button
+            icon={<EditOutlined />}
+            type="text"
+            style={{ width: 100, height: 100 }}
+            shape="circle"
+            onClick={() => {
+              document.getElementById("infile").click();
+            }}
+          ></Button>
+        </div>
+        <Avatar src={avatar} style={{ width: 100, height: 100 }} />
+      </div>
+    </>
+  );
+}
+
+export default AvatarUpload;
